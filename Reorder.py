56a6d5fca6789d55fe221871a2ccf536f6a3b36f
Matteo <matteo.lubi@gmail.com>
2018-06-23 20:07:42 +0200

    Added moving for pdf and video

--- Reorder.py
+++ Reorder.py
@@ -1,26 +1,47 @@
-import os
 import time
+import glob
+import shutil
+import os
+
+pathToSearchIn = '/Users/matteolubinu/Downloads'
 
 def init():
-    folderSize = os.path.getsize("C:/Users/IBM_ADMIN/Downloads")
+    folderSize = len([file for file in glob.glob(glob.escape(pathToSearchIn) + '/**/*', recursive=True)])
     return folderSize
 
-def sizeChecker(folderSize=null, newFolderSize):
-    
-    if folderSize:
-        oldFolderSize = folderSize
-    if newFolderSize:
-        oldFolderSize = newFolderSize
-    print("Folder size (old data):" , folderSize/1073741824 , "Gb")
+def sizeChecker(folderSize):
+    oldFolderSize = folderSize
+    print("Folder size (old size):" , oldFolderSize , "Elements" )
     time.sleep(10) #in seconds
-    newFolderSize = os.path.getsize("C:/Users/IBM_ADMIN/Downloads")
-    
-    print("Folder size (new data):" , newFolderSize/1073741824 , "Gb")
+    newFolderSize = len([file for file in glob.glob(glob.escape(pathToSearchIn) + '/**/*', recursive=True)])
+    print("Folder size (new size):" , newFolderSize , "Elements")
     if newFolderSize > oldFolderSize:
         print ("Changes detected")
-    return newFolderSize
+        newFile = ""
+        newFile = divideFiles()
+        if newFile.endswith('.pdf'): #PDF file if
+            destination = '/Users/matteolubinu/Libri' #Change the folder destination
+            shutil.move(newFile, destination)
+            main()
+        if newFile.endswith('.mkv') or newFile.endswith('.mp4') : #Video file if
+            destination = '/Users/matteolubinu/Video' #Change the folder destination
+            shutil.move(newFile, destination)
+            main()
+    else: 
+        print ("No changes")
     sizeChecker(newFolderSize) #run again the function after time set
 
-init()
-sizeChecker()  
+def divideFiles():
+   latestFile = ""
+   listOfFiles = glob.glob('/Users/matteolubinu/Downloads/*')
+   latestFile = sorted(listOfFiles, key=os.path.getmtime)[0]
+   print(latestFile)
+   return latestFile
+    
+def main():
+    folderSize = init()
+    sizeChecker(folderSize)  
+
+main()
+
     
